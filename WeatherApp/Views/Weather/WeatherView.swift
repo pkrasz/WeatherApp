073f58907326c2cf5e4a27a42add5b980a4d669c
pkrasz <pl.kraszewski@gmial.com>
2022-03-31 15:29:19 +0200

    rest

---
+++ WeatherApp/Views/Weather/WeatherView.swift
@@ -0,0 +1,45 @@
+//
+//  WeatherView.swift
+//  WeatherApp
+//
+//  Created by Paweł Kraszewski on 31/03/2022.
+//
+
+import UIKit
+
+class WeatherView: UIView {
+    
+    //MARK: - SubView
+    
+    //MARK: - Initializator
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupView()
+        setupSubview()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Setup
+    
+    private func setupView() {
+        backgroundColor = .white
+    }
+    
+    private func setupSubview() {
+        
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            
+        ])
+    }
+}
+
+
