a2c6507623e728e8347057d11a56f6d9f0907be7
pkrasz <pl.kraszewski@gmial.com>
2022-03-31 15:29:04 +0200

    add EnterView

--- WeatherApp/Views/EnterView.swift
+++ WeatherApp/Views/EnterView.swift
@@ -8,37 +8,88 @@
 import UIKit
 
 class EnterView: UIView {
-
+    
     //MARK: - SubView
-       
-        //MARK: - Initializator
-
-        override init(frame: CGRect) {
-            super.init(frame: frame)
-            
-            setupView()
-            setupSubview()
-            setupConstraints()
-        }
+    let logoImageView: UIImageView = {
+        let image = UIImageView(image: UIImage(systemName: "cloud.sun.fill"))
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.tintColor = .systemBlue
+        return image
+    }()
+    let cityLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Podaj miasto w jakim chcesz sprawdzić pogodę!"
+        label.textColor = .white
+        label.font = .boldSystemFont(ofSize: 15)
+        return label
+    }()
+    let cityTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.white.cgColor
+        textField.textColor = .white
+        return textField
+    }()
+    let checkButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemBlue
+        button.setTitle("Sprawdź!", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        return button
+    }()
+    
+    //MARK: - Initializator
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         
-        required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
-        
-        //MARK: - Setup
-        
-        private func setupView() {
-            backgroundColor = .white
-        }
-        
-        private func setupSubview() {
-        
-        }
-        
-        private func setupConstraints() {
-            NSLayoutConstraint.activate([
-                
-           ])
-        }
+        setupView()
+        setupSubview()
+        setupConstraints()
     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Setup
+    
+    private func setupView() {
+        backgroundColor = .systemCyan
+    }
+    
+    private func setupSubview() {
+        addSubview(logoImageView)
+        addSubview(cityLabel)
+        addSubview(cityTextField)
+        addSubview(checkButton)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10),
+            logoImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            logoImageView.widthAnchor.constraint(equalToConstant: 250),
+            logoImageView.heightAnchor.constraint(equalToConstant: 180),
+            
+            cityLabel.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 50),
+            cityLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            cityTextField.topAnchor.constraint(equalTo: cityLabel.bottomAnchor, constant: 10),
+            cityTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 100),
+            cityTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -100),
+            
+            checkButton.topAnchor.constraint(equalTo: cityTextField.bottomAnchor, constant: 50),
+            checkButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            checkButton.widthAnchor.constraint(equalToConstant: 200),
+            checkButton.heightAnchor.constraint(equalToConstant: 50)
+            
+            
+        ])
+    }
+}
 
---
+++ WeatherApp/Views/Weather/WeatherViewController.swift
@@ -0,0 +1,42 @@
+//
+//  WeatherViewController.swift
+//  WeatherApp
+//
+//  Created by Paweł Kraszewski on 31/03/2022.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+    var contentView: EnterView {
+        return view as! EnterView
+    }
+    
+    //MARK: - Lifecycle
+    
+    override func loadView() {
+        view = EnterView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+        setupBindings()
+    }
+    
+    //MARK: - Setup
+    
+    private func setupView() {
+        
+    }
+    
+    private func setupBindings() {
+        
+    }
+}
+
+
